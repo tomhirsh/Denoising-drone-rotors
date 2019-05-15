50427c3f591c5737767e9dbace464521312a72f7
Tom Hirshberg <31562517+tomhirsh@users.noreply.github.com>
2019-05-15 10:36:40 +0300

    template for every possible channel size

--- models/deep_isp_model.py
+++ models/deep_isp_model.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import actquant
 import quantize
-from layers import ResConvLayer , Conv2d
+from layers import ResConvLayer , Conv2d, NUM_CHANNELS
 from uniq import UNIQNet
 #from quantize import quantize
 
@@ -20,12 +20,10 @@ class DenoisingNet(UNIQNet):
                  bitwidth=32, use_cuda=True, quant_edges=True, act_quant = False, act_noise=False, act_bitwidth=8 , quant_epoch_step=80,
                  quant_start_stage=0, weight_relu=False,weight_grad_after_quant=False, random_inject_noise = False, std_weight_clamp=2.3,std_act_clamp=6.9,step=19, wrpn=False,quant_first_layer=True,num_of_layers_each_step=2):
         """DenoisingNet constructor.
-
         Arguments:
             in_channels (int, optional): number of channels in the input tensor. Default is 3 for RGB image inputs.
             num_denoise_layers (int, optional): number of denoising layers
             num_filters (int, optional): number of filters in denoising layers
-
         """
 
         super(DenoisingNet, self).__init__(quant_epoch_step=quant_epoch_step,quant_start_stage=quant_start_stage, quant=quant, noise=noise, bitwidth=bitwidth, step=step, quant_edges=quant_edges,
@@ -40,7 +38,7 @@ class DenoisingNet(UNIQNet):
         self.in_channels = in_channels
 
         self.input_padder = nn.ReflectionPad2d(1)
-        self.conv1 = Conv2d(in_channels, num_filters - 3, kernel_size=3)
+        self.conv1 = Conv2d(in_channels, num_filters-NUM_CHANNELS, kernel_size=3)
 
         self.nonlinearity1 = self.get_feature_activation()
 
@@ -83,23 +81,24 @@ class DenoisingNet(UNIQNet):
             modules[name] = module
         # Last module has different number of outputs
         layer_num = self.num_denoise_layers - 1
-        denoise_layer = ResConvLayer(self.num_filters, self.in_channels, self.get_feature_activation(), 'reflect', None, act_quant = self.act_quant, act_bitwidth=self.act_bitwidth)
-        modules[stage_name + str(layer_num)] = denoise_layer
+        denoise_layer = ResConvLayer(self.num_filters, NUM_CHANNELS, self.get_feature_activation(), 'reflect', nn.Sigmoid, act_quant = self.act_quant, act_bitwidth=self.act_bitwidth)
+        modules[stage_name + "_{}".format(layer_num)] = denoise_layer
 
         return nn.Sequential(modules)
 
     def forward(self, image):
 
         x = self.nonlinearity1(self.conv1(self.input_padder(image)))
+        # First feature layer contains rpm, the rest is single channel 
+        image = image[:,:NUM_CHANNELS,:]
         image_out = self.denoising(torch.cat((x, image), 1))
-
         # Quantizing input image
         image_quant_scale = (2**16-1)/(torch.min(image) - torch.max(image))
         quant_image = torch.round(image_quant_scale * image)/image_quant_scale
 
         final_image = image_out + quant_image
+        # todo: try to add sigmoid to output to get 0-1 output
+        # final_image = torch.sigmoid(final_image)
         # final_image = image_out + image
 
         return final_image
-
-
